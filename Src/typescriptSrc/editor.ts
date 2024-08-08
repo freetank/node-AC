1b228b0dad45decdb8f2d488333b88b8d1d3cd0a
Mihaly Palenik <palenik.mihaly@gmail.com>
2024-08-08 11:33:57 +0200

    Extend the editor.ts based on the example tutorial: https://retejs.org/docs/guides/basic Not working yet.

--- Src/typescriptSrc/editor.ts
+++ Src/typescriptSrc/editor.ts
@@ -1,22 +1,66 @@
-import { NodeEditor, GetSchemes, ClassicPreset } from "rete";
 import { createRoot } from "react-dom/client";
-import { AreaPlugin } from "rete-area-plugin";
+import { NodeEditor, GetSchemes, ClassicPreset } from "rete";
+import { AreaPlugin, AreaExtensions } from "rete-area-plugin";
+import {
+  ConnectionPlugin,
+  Presets as ConnectionPresets,
+} from "rete-connection-plugin";
 import { ReactPlugin, Presets, ReactArea2D } from "rete-react-plugin";
 
 type Schemes = GetSchemes<
   ClassicPreset.Node,
   ClassicPreset.Connection<ClassicPreset.Node, ClassicPreset.Node>
 >;
-
-const editor = new NodeEditor<Schemes>();
-
 type AreaExtra = ReactArea2D<Schemes>;
 
-const container = document.getElementById("container")!;
-const area = new AreaPlugin<Schemes, AreaExtra>(container);
-const render = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
+export async function createEditor(container: HTMLElement) {
+  const socket = new ClassicPreset.Socket("socket");
 
-render.addPreset(Presets.classic.setup());
+  const editor = new NodeEditor<Schemes>();
+  const area = new AreaPlugin<Schemes, AreaExtra>(container);
+  createRoot (container);
+  const connection = new ConnectionPlugin<Schemes, AreaExtra>();
+  const render = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
 
-editor.use(area);
-area.use(render);
+  AreaExtensions.selectableNodes(area, AreaExtensions.selector(), {
+    accumulating: AreaExtensions.accumulateOnCtrl(),
+  });
+
+  render.addPreset(Presets.classic.setup());
+
+  connection.addPreset(ConnectionPresets.classic.setup());
+
+  editor.use(area);
+  area.use(connection);
+  area.use(render);
+
+  AreaExtensions.simpleNodesOrder(area);
+
+  const a = new ClassicPreset.Node("A");
+  a.addControl("a", new ClassicPreset.InputControl("text", { initial: "a" }));
+  a.addOutput("a", new ClassicPreset.Output(socket));
+  await editor.addNode(a);
+
+  const b = new ClassicPreset.Node("B");
+  b.addControl("b", new ClassicPreset.InputControl("text", { initial: "b" }));
+  b.addInput("b", new ClassicPreset.Input(socket));
+  await editor.addNode(b);
+
+  await editor.addConnection(new ClassicPreset.Connection(a, "a", b, "b"));
+
+  await area.translate(a.id, { x: 0, y: 0 });
+  await area.translate(b.id, { x: 270, y: 0 });
+
+  setTimeout(() => {
+    // wait until nodes rendered because they dont have predefined width and height
+    AreaExtensions.zoomAt(area, editor.getNodes());
+  }, 10);
+  return {
+    destroy: () => area.destroy(),
+  };
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const container = document.getElementById("container")!;
+  createEditor (container);
+});
